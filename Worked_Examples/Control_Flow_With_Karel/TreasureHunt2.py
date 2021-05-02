66b9ab33b72e98bfd8ad67482e41b5ae82b00f91
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-02 09:28:46 -0700

    updated

---
+++ Worked_Examples/Control_Flow_With_Karel/TreasureHunt2.py
@@ -0,0 +1,31 @@
+from karel.stanfordkarel import *
+
+"""
+File: TreasureHunt2.py
+------------------------------
+Karel will move forward until a beeper.
+At every beeper, Karel will turn left and move until the next beeper, until Karel is standing on the treasure, which is a pile of 10 beepers.
+"""
+
+def main():
+    # Solves treasure map.
+    while no_beepers_present():
+        move_to_beeper()
+        pick_beeper()
+        turn_left()
+
+    # Collect treasure!
+    while beepers_present():
+        pick_beeper()
+
+    # This for loop also works:
+    # for i in range(9):
+    #     pick_beeper()
+
+def move_to_beeper():
+    """ Moves Karel forward until Karel is standing on a beeper. """
+    while no_beepers_present():
+        move()
+
+if __name__ == "__main__":
+    run_karel_program('TreasureHunt2.w')
---
+++ Worked_Examples/Control_Flow_With_Karel/TreasureHunt2.w
@@ -0,0 +1,22 @@
+Dimension: (7, 7)
+Wall: (6, 5); south
+Wall: (2, 6); east
+Beeper: (6, 1); 0
+Beeper: (5, 1); 0
+Beeper: (5, 6); 0
+Beeper: (1, 6); 0
+Beeper: (1, 5); 0
+Beeper: (7, 5); 0
+Beeper: (7, 7); 0
+Beeper: (3, 7); 0
+Beeper: (3, 3); 0
+Beeper: (3, 1); 1
+Beeper: (6, 2); 10
+Beeper: (6, 4); 1
+Beeper: (2, 4); 1
+Beeper: (3, 6); 1
+Beeper: (2, 7); 1
+Beeper: (6, 7); 1
+Beeper: (6, 6); 1
+Karel: (1, 1); east
+
