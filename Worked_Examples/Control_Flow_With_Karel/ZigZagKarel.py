2497acbc421a76c4c4ca427538702448781bcdd9
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-02 08:04:33 -0700

    updated

--- Worked_Examples/Control_Flow_With_Karel/ZigZagKarel.py
+++ Worked_Examples/Control_Flow_With_Karel/ZigZagKarel.py
@@ -7,6 +7,31 @@ Places a zig zag pattern of beepers in the world.
 """
 
 def main():
+    if front_is_clear():
+        zig()
+        zag()
+        if front_is_blocked():
+            put_beeper()
+def zig():
+    put_beeper()
+    move()
+    turn_left()
+    move()
+    turn_right()
+def zag():
+    put_beeper()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+def zigzag():
+    if right_is_clear():
+        turn_right()
+        move()
+    else:
+        turn_left()
+        move()
 
 def turn_right():
     turn_left()
