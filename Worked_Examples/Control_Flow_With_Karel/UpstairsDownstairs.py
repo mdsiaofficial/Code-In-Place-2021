995a6b983e1beb0d31fcbbabb356a41a8d4ca758
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-02 22:43:40 -0700

    updated

--- Worked_Examples/Control_Flow_With_Karel/UpstairsDownstairs.py
+++ Worked_Examples/Control_Flow_With_Karel/UpstairsDownstairs.py
@@ -7,7 +7,29 @@ Karel will climb three stair steps up and then three stair steps down.
 """
 
 def main():
-    while
+    climb_mountain()
+
+
+
+def climb_mountain():
+    while front_is_blocked():
+        step_up()
+        step_up()
+
+def step_up():
+    # karel must be facing east
+    turn_left()
+    move()
+    turn_right()
+    move()
+
+
+def turn_right():
+    for _ in range(3):
+        turn_left()
+
+
+
 
 
 
