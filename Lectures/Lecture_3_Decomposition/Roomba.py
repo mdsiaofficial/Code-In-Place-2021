f6016e8107093b0bfba988f1f3afa988db4302ef
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-05 01:43:01 +0600

    updated

--- Lectures/Lecture_3_Decomposition/Roomba.py
+++ Lectures/Lecture_3_Decomposition/Roomba.py
@@ -11,8 +11,10 @@ def main():
         move_to_wall()
         clear_row()
         # karel is facing east in the next row
-        return_to_first_col()
+        # return_to_first_col()
+
         next_row()
+
     clear_row()
 
 
