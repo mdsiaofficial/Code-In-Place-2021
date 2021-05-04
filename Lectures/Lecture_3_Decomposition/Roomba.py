ad37906894bf7aef49271f5a41c0ff6f1ee1be04
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-05 01:41:59 +0600

    updated

--- Lectures/Lecture_3_Decomposition/Roomba.py
+++ Lectures/Lecture_3_Decomposition/Roomba.py
@@ -7,12 +7,20 @@ Karel picks up all the beepers in the world.
 """
 
 def main():
+    while left_is_clear():
+        move_to_wall()
+        clear_row()
+        # karel is facing east in the next row
+        return_to_first_col()
+        next_row()
+    clear_row()
+
 
 
 # Removes all beepers from one row
 def clear_row():
     while front_is_clear():
-        safe_pick()
+        safe_pick()   #ITS own function
         move()
     safe_pick()
 
@@ -38,6 +46,9 @@ def move_to_wall():
 def turn_around():
     turn_left()
     turn_left()
+def return_to_first_col():
+    turn_around()
+    move_to_wall()
 
 # Recall, not an ambi turner
 def turn_right():
@@ -45,4 +56,4 @@ def turn_right():
         turn_left()
 
 if __name__ == "__main__":
-    run_karel_program()
+    run_karel_program('Roomba1.w')
