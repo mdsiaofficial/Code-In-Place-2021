b3ef9b6073f97fda239c739f6641d3b6a26dae0d
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-05 01:16:22 +0600

    updated

---
+++ Lectures/Lecture_4_Introduction_To_Python/add2numbers.py
@@ -0,0 +1,13 @@
+"""
+Another python program to get some practice with variables.
+This program asks the user for two integers and prints their sum.
+"""
+
+def main():
+
+# This provided line is required at the end of a Python file
+# to call the main() function.
+
+
+if __name__ == '__main__':
+    main()
---
+++ Lectures/Lecture_4_Introduction_To_Python/helloworld.py
@@ -0,0 +1,14 @@
+"""
+This is our first python program. It is customary to
+have a programmer's first program write "hello world"
+(inspired by the first program in Brian Kernighan and
+Dennis Ritchie's classic book, 'The C Programming Language.')
+"""
+
+def main():
+
+
+# This provided line is required at the end of a Python file
+# to call the main() function.
+if __name__ == '__main__':
+    main()
