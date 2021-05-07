c9b7e387ebb1defe03e37ca328b9e7ab62381659
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-07 16:04:39 +0600

    updated

--- Lectures/Lecture_5_Expressions/average2numbers.py
+++ Lectures/Lecture_5_Expressions/average2numbers.py
@@ -3,13 +3,11 @@ average2numbers
 This program asks the user for two numbers and prints their average.
 """
 
-
 def main():
     first_number = input('Gimme 1st number: ')
     second_number = input('Gimme 2nd number: ')
     average = (int(first_number)+int(second_number))/2
     print('Your Average is: ' + average)
-
 
 if __name__ == '__main__':
     main()
