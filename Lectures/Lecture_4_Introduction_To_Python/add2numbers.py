9d2f38ed70c6287891450dec7aa1402de4f2a724
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-07 15:27:26 +0600

    updated

--- Lectures/Lecture_4_Introduction_To_Python/add2numbers.py
+++ Lectures/Lecture_4_Introduction_To_Python/add2numbers.py
@@ -6,9 +6,10 @@ This program asks the user for two integers and prints their sum.
 def main():
 
     print('Hello there')
-    user_input = input('Whats you name?')
-    print('Hi, Shoriful' + user_input)
-
+    ## user_input = input('Whats you name?')
+    ## print('Hi, ' + user_input)
+    x = 42
+    print(x/2)
 # This provided line is required at the end of a Python file
 # to call the main() function.
 if __name__ == '__main__':
