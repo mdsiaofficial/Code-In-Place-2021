6be63c882832ff8c3267710f9fbc8ff23aa1e7fc
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-07 21:30:10 +0600

    updated

--- Lectures/Lecture_5_Expressions/constant.py
+++ Lectures/Lecture_5_Expressions/constant.py
@@ -7,7 +7,9 @@ An example program with constants that converts feet to inches
 inches_in_foot = 12
 
 def main():
-
+    feet = float(input("Enter number of feet: "))
+    inches = feet * inches_in_foot
+    print("Here its ", inches, "inches!")
 
 
 if __name__ == '__main__':
