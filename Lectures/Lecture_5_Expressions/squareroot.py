87ee11fcb8b2cea25bcd4f34f85158f09a33c905
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-07 21:35:59 +0600

    updated

--- Lectures/Lecture_5_Expressions/squareroot.py
+++ Lectures/Lecture_5_Expressions/squareroot.py
@@ -6,8 +6,8 @@ This program computes square roots
 #import here
 import math
 def main():
-    num = int(input("Enter a number: ")
-    print(math.sqrt(num))
+    num = int(input("Enter a number: "))
+    print(int(math.sqrt(num)))
 
 if __name__ == '__main__':
     main()
