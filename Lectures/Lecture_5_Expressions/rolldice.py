ee454eafc247cce902f5152ccdde72d804ec5b1a
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-07 21:53:10 +0600

    updated

--- Lectures/Lecture_5_Expressions/rolldice.py
+++ Lectures/Lecture_5_Expressions/rolldice.py
@@ -2,13 +2,19 @@
 
 """
 
-#import here
-import random
+# Number of sides on each die to roll
+NUM_SIDES = 6
+
 def main():
-    x = 10
-    print(random.seed(x))
-
-
+    # setting seed is useful for debugging
+    # random.seed(1)
+    die1 = random.randint(1, NUM_SIDES)
+    die2 = random.randint(1, NUM_SIDES)
+    total = die1 + die2
+    print("Dice have", NUM_SIDES, "sides each.")
+    print("First die:", die1)
+    print("Second die:", die2)
+    print("Total of two dice:", total)
 
 
 if __name__ == '__main__':
