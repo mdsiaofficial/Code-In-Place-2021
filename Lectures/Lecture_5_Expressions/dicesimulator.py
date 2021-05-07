3d8a9d219e40d6356d05042d2ae680601042acce
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-07 21:59:32 +0600

    updated

--- Lectures/Lecture_5_Expressions/dicesimulator.py
+++ Lectures/Lecture_5_Expressions/dicesimulator.py
@@ -1,11 +1,15 @@
 """
+dicesimulator
+This program simulates rolling two dice, three times. It prints the results of each die roll.
 
+This program is used to show how variable scope works.
 """
 
 #import here
-import
+import random
 def main():
-
+    die1 = 10
+    print("die1 in main() starts as: " + str(die1))
 
 if __name__ == '__main__':
     main()
