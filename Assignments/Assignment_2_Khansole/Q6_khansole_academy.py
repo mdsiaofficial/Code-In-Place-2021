2d47601bd133e66c58806927718b3c8c6e2598aa
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-09 15:49:19 +0600

    updated

--- Assignments/Assignment_2_Khansole/Q6_khansole_academy.py
+++ Assignments/Assignment_2_Khansole/Q6_khansole_academy.py
@@ -6,7 +6,11 @@ and checks if the user answers correctly.
 import random 
 
 def main():
-    pass
+    num1 = random.randint(10,99)
+    num2 = random.randint(10,99)
+    sum = num1+num2
+
+    print(sum)
 
 if __name__ == '__main__':
     main()
