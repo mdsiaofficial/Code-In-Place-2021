895ce69623edfd14ceadc94e2460a319cad47e03
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-09 15:46:00 +0600

    updated

--- Assignments/Assignment_2_Khansole/Q5_liftoff.py
+++ Assignments/Assignment_2_Khansole/Q5_liftoff.py
@@ -3,7 +3,11 @@ Prints out a spaceship launch sequence.
 """
 
 def main():
-    pass
+    a = int(10)
+    while a!=0:
+        print(a)
+        a-=1
+    print("Liftoff!")
 
 if __name__ == '__main__':
     main()
