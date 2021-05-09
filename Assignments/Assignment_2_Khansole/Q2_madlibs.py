4bf1628ca53f66e09ab4f98d97ef3938aba91630
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-09 09:06:45 +0600

    updated

--- Assignments/Assignment_2_Khansole/Q2_madlibs.py
+++ Assignments/Assignment_2_Khansole/Q2_madlibs.py
@@ -17,6 +17,6 @@ def main():
     print(WIZARD + " realized they couldn't keep all those " + FRUIT + " to themselves,")
     print("so they sold them at the market for $" + str(PRICE) + " apiece,")
     print("and with the earnings bought fruit to share with the entire village!")
-
+    print("Legend says " + str(YEARS) + " years later, " + str(WIZARD) + " is still eating fruit.")
 if __name__ == '__main__':
     main()
