136ede0dce1601b4152152d28fcc3dbe31ae1769
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-09 17:39:38 +0600

    updated

--- Assignments/Assignment_2_Khansole/Q7_khansole_academy (optional).py
+++ Assignments/Assignment_2_Khansole/Q7_khansole_academy (optional).py
@@ -3,27 +3,24 @@ TODO: Write a description here
 """
 
 import random
-cmplt_prgrm = 3
-
 
 def main():
-
-    num1 = random.randint(10,99)
-    num2 = random.randint(10,99)
-    sum = num1+num2
-    correct_count = 0
-    print("What is "+str(num1)+"+"+str(num2)+"?")
-    user_input = int(input("Your answer: "))
-
-    while correct_count !=cmplt_prgrm:
-        if user_input==sum and correct_count<cmplt_prgrm:
-            print("Correct!")
+    correct_count=0
+    while True:
+        num1 = random.randint(10,99)
+        num2 = random.randint(10,99)
+        sum = num1+num2
+        print("What is "+str(num1)+"+"+str(num2)+"?")
+        user_input = int(input("Your answer: "))
+        if user_input==sum:
             correct_count += 1
+            print("Correct! You've gotten ",correct_count, "correct in a row.")
         else:
-            print("Incorrect. The expected answer is "+str(sum))
-            print("What is " + str(num1) + "+" + str(num2) + "?")
-            user_input = int(input("Your answer: "))
-    print(Correct! You've gotten 3 correct in a row.')
-    print("Congratulations! You mastered addition.")
+            print("Incorrect. The expected answer is ",sum)
+
+        if correct_count==3:
+            print("Congratulations! You mastered addition.")
+            break
+
 if __name__ == '__main__':
     main()
