f16214b3b042dff292f7b405cce32a4628d45366
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-09 16:00:19 +0600

    updated

--- Assignments/Assignment_2_Khansole/Q7_khansole_academy (optional).py
+++ Assignments/Assignment_2_Khansole/Q7_khansole_academy (optional).py
@@ -3,9 +3,18 @@ TODO: Write a description here
 """
 
 import random
-
+cmplt_prgrm = 3
 def main():
-    pass
+
+    num1 = random.randint(10,99)
+    num2 = random.randint(10,99)
+    sum = num1+num2
+    print("What is "+str(num1)+"+"+str(num2)+"?")
+    user_input = int(input("Your answer: "))
+    if user_input==sum:
+        print("Correct!")
+    else:
+        print("Incorrect. The expected answer is "+str(sum))
 
 if __name__ == '__main__':
     main()
