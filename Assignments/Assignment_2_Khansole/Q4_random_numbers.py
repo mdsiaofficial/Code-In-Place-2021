85ac56935d7c356db305c4a8b0afa97f496d7899
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-09 09:17:21 +0600

    updated

--- Assignments/Assignment_2_Khansole/Q4_random_numbers.py
+++ Assignments/Assignment_2_Khansole/Q4_random_numbers.py
@@ -3,10 +3,12 @@ Prints out 10 random numbers between 0 and 100.
 """
 
 import random
-
+NUM_RANDOM = 10
+MIN_RANDOM = 0
+MAX_RANDOM = 100
 def main():
-    for _ in range(10):
-        print(random.randint(0,100))
+    for _ in range(NUM_RANDOM):
+        print(random.randint(MIN_RANDOM,MAX_RANDOM))
 
 if __name__ == '__main__':
     main()
