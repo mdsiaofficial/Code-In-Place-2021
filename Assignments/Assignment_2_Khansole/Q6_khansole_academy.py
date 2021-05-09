0288fc1407b365ff47630dc9246153c447c3cff3
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-09 15:53:27 +0600

    updated

--- Assignments/Assignment_2_Khansole/Q6_khansole_academy.py
+++ Assignments/Assignment_2_Khansole/Q6_khansole_academy.py
@@ -9,12 +9,12 @@ def main():
     num1 = random.randint(10,99)
     num2 = random.randint(10,99)
     sum = num1+num2
-    print("What is "+num1+"+"+num2+"?")
+    print("What is "+str(num1)+"+"+str(num2)+"?")
     user_input = int(input("Your answer: "))
     if user_input==sum:
         print("Correct!")
     else:
-        print("Incorrect. The expected answer is "+sum)
+        print("Incorrect. The expected answer is "+str(sum))
 
 if __name__ == '__main__':
     main()
