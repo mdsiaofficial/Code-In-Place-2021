19704fb465ab7741bd76ade0b40ecd5ffd27f872
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-10 23:45:04 +0600

    updated

---
+++ Assignments/Assignment_2_Khansole/Q8_nimm.py
@@ -0,0 +1,20 @@
+"""
+The game of Nimm goes as follows:
+
+The game starts with a pile of 20 stones between the players.
+
+The two players alternate turns.
+
+On a given turn, a player may take either 1 or 2 stone from the center pile.
+
+The two players continue until the center pile has run out of stones.
+"""
+
+import random
+
+def main():
+
+    stones = 20
+
+if __name__ == '__main__':
+    main()
