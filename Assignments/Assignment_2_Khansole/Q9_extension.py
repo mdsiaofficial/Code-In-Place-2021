d1a97afff8bbffb439bdad2f2af63cb15e4c2cb0
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-11 22:49:52 +0600

    updated

---
+++ Assignments/Assignment_2_Khansole/Q9_extension.py
@@ -0,0 +1,15 @@
+"""
+Hailstones
+A separate (optional) problem you could consider writing is based on a problem in Douglas Hofstadter’s Pulitzer-prize-winning book Gödel, Escher, Bach. That book contains many interesting mathematical puzzles, many of which can be expressed in the form of computer programs. In Chapter XII, Hofstadter mentions a wonderful problem that is well within the scope of what you know. The problem can be expressed as follows:
+
+Pick some positive integer and call it n.
+If n is even, divide it by two.
+If n is odd, multiply it by three and add one.
+Continue this process until n is equal to one.
+"""
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
