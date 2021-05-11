4fe92bb54da59cab44816ce35a0c91ab49ee232c
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-11 13:36:25 +0600

    updated

--- Assignments/Assignment_2_Khansole/Q8_nimm.py
+++ Assignments/Assignment_2_Khansole/Q8_nimm.py
@@ -13,18 +13,24 @@ The two players continue until the center pile has run out of stones.
 import random
 
 def main():
-    stones=20
-    player=1
+
+    stones=20   # stones that appears before starting game
+    player=1    # dummy player count, it will increase and decrease
 
     while stones>0:
+        # this outputs as asked in the description
         print("There are",stones,"stones left")
         removed_stones=int(input("Player " +str(player)+" would you like to remove 1 or 2 stones? "))
 
+        # this loop for checking user inputs other than 1 or 2
         while removed_stones!=1 and removed_stones!=2:
-            removed_stones=0
+            removed_stones = 0
             removed_stones=int(input("Please enter 1 or 2: "))
         print("")
+
         stones-=removed_stones
+
+
         if player ==1:
             player+=1
         else:
