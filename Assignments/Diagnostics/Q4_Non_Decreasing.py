5521d44a83325c19f2e358be4e3c8a9f97312f56
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-12 22:22:12 +0600

    updated

--- Assignments/Diagnostics/Q4_Non_Decreasing.py
+++ Assignments/Diagnostics/Q4_Non_Decreasing.py
@@ -1,6 +1,16 @@
+"""
 
+"""
 
 def main():
+    print("Enter a sequence of non-decreasing numbers.")
+    value = input("Enter num: ")
+    previous_value = value
+    while value >= previous_value:
+        value = input("Enter num: ")
+
+
+
 
 if __name__ == "__main__":
     run_karel_program()
