a27f195bb16fdc7cccf896658293ee4a42c45c41
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-12 21:59:55 +0600

    updated

--- Assignments/Diagnostics/Even_Odd_Table.py
+++ Assignments/Diagnostics/Even_Odd_Table.py
@@ -6,14 +6,16 @@ Print out each of the numbers 1 through 100 and whether that number is even or o
 Here is what the output looks like when MAX_NUMBER = 100
 """
 
-MAX_NUMBER = 100
 
+MAX_NUMBER = 100
+MIN_NUMBER = 1
 def main():
-    for i in range(MAX_NUMBER):
-        if i%2 == 0:
+    while MIN_NUMBER <= 100:
+        if MIN_NUMBER % 2 == 0:
             print(i, "is even")
         else:
             print(i, "is odd")
+        MIN_NUMBER += 1
 
 if __name__ == '__main__':
     main()
