8d539b7915d48719d09cf5d027859ca0069a7cc7
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-12 22:37:12 +0600

    updated

--- Assignments/Diagnostics/Q4_Non_Decreasing.py
+++ Assignments/Diagnostics/Q4_Non_Decreasing.py
@@ -3,14 +3,15 @@
 """
 
 def main():
+
     print("Enter a sequence of non-decreasing numbers.")
     value = input("Enter num: ")
+    # here its defining the prev of main value
     previous_value = value
-    sequence_length = 0
+    sequence_length = 0     # its steps need to do
     while value >= previous_value:
         sequence_length += 1
         previous_value = value
-
         value = input("Enter num: ")
     print("Thanks for playing!")
     print("Sequence length:", sequence_length)
