cdd9121514b7b12e45cb8a6c05e375f2e6042a76
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-24 23:21:14 -0700

    updated

--- Assignments/Assignment_3_Images/warhol_filter.py
+++ Assignments/Assignment_3_Images/warhol_filter.py
@@ -4,19 +4,12 @@ This program generates the Warhol effect based on the original image.
 
 from simpleimage import SimpleImage
 import random
-
 N_ROWS = 2
 N_COLS = 3
 PATCH_SIZE = 222
 WIDTH = N_COLS * PATCH_SIZE
 HEIGHT = N_ROWS * PATCH_SIZE
-PATCH_NAME = 'images/my_cat.jpg'
-
-def main():
-    image = SimpleImage(PATCH_NAME)
-    warhols_image = SimpleImage.blank(WIDTH, HEIGHT)
-    set_all_patchs(warhols_image)
-    warhols_image.show()
+PATCH_NAME = 'images/simba-sq.jpg'
 
 def make_recolored_patch(red_scale, green_scale, blue_scale):
     '''
@@ -25,37 +18,38 @@ def make_recolored_patch(red_scale, green_scale, blue_scale):
     :param red_scale: A number to multiply each pixel's red component by
     :param green_scale: A number to multiply each pixel's green component by
     :param blue_scale: A number to multiply each pixel's blue component by
-     Returns the newly generated patch.
+    Returns the newly generated patch.
     '''
     patch = SimpleImage(PATCH_NAME)
+    # TODO: your code here
     for pixel in patch:
-        pixel.red *= red_scale
-        pixel.green *= green_scale
-        pixel.blue *= blue_scale
+        pixel.red = pixel.red * red_scale
+        pixel.green = pixel.green * green_scale
+        pixel.blue = pixel.blue * blue_scale
     return patch
 
-def set_all_patchs(warhols_image):
-   """
-   Iterating the rows and columns, and for each cell we will add a PATH.
-   Before add a path we are put a single path into a cell
-   """
-   for x in range(N_COLS):
-      for y in range(N_ROWS):
-         patch = make_recolored_patch(random.random()*1.5, random.random()*1.5, random.random()*1.5)
-         put_patch(patch, x, y, warhols_image)
 
-def put_patch(patch, start_x, start_y , warhols_image):
-   """
-   Getting the position of the single pixel in our blank image (warhols_image), then we are filling
-   each pixel with the image. This for each pixel in the blank image of size: 222 and 222
-   """
-   start_x = start_x * PATCH_SIZE
-   start_y = start_y * PATCH_SIZE
+def put_patch(final_image, start_x, start_y, patch):
 
-   for x in range (PATCH_SIZE):
-      for y in range (PATCH_SIZE):
-         pixel = patch.get_pixel(x, y)
-         warhols_image.set_pixel(x + start_x, y + start_y, pixel)
+    for x in range(PATCH_SIZE):
+        for y in range(PATCH_SIZE):
+            pixel = patch.get_pixel(x,y)
+            final_image.set_pixel(x+start_x,y + start_y,pixel)
+
+
+def main():
+    final_image = SimpleImage.blank(WIDTH, HEIGHT)
+
+    # TODO: your code here
+    # This is an example which should generate a pinkish patch
+
+    for row in range(N_ROWS):
+        for col in range(N_COLS):
+            start_x = col * PATCH_SIZE
+            start_y = row * PATCH_SIZE
+            patch = make_recolored_patch(random.random()*1.5, random.random()*1.5, random.random()*1.5)
+            put_patch(final_image, start_x, start_y , patch)
+    final_image.show()
 
 if __name__ == '__main__':
     main()
