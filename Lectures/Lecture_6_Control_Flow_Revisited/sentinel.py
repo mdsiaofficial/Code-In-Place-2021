05e5bcbcba1f184ba1ce3b513605fdf3ce7cbdc9
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-05-28 07:53:54 -0700

    updated

---
+++ Lectures/Lecture_6_Control_Flow_Revisited/sentinel.py
@@ -0,0 +1,11 @@
+"""
+Asks the user for a number until they enter -1
+Reports the total value of all the numbers at the end
+"""
+
+def main():
+    while True:
+        num = input("Type a number: ")
+
+if __name__ == '__main__':
+    main()
