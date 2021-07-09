3897a48c418abed391074a8051667e68c97ce67a
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-07-09 08:54:49 +0600

    Create ages.py

---
+++ Worked_Examples/Welcome_To_Python/ages.py
@@ -0,0 +1,16 @@
+def main():
+	anton = 21
+	beth = 6
+	chen = 20
+	drew = int(anton) + int(chen)
+	ethan = 20
+	
+	print(" Anton is ",anton,"years old")
+	print(" Anton is ",beth,"years old")
+	print(" Anton is ",chen,"years old")
+	print(" Anton is ",drew,"years old")
+	print(" Anton is ",ethan,"years old")
+	
+	
+if __name__ == '__main__':
+	main()
